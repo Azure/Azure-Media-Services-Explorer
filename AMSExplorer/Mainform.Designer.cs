16009e3c1e0c90c7a71036845670e4a568351f15
xpouyat <xpouyat@microsoft.com>
2015-01-23 17:00:44 +0100

    Updated code to delete assets and jobs

--- AMSExplorer/Mainform.Designer.cs
+++ AMSExplorer/Mainform.Designer.cs
@@ -1063,7 +1063,6 @@
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
             this.deleteToolStripMenuItem.Size = new System.Drawing.Size(346, 22);
             this.deleteToolStripMenuItem.Text = "Delete...";
-            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
             // 
             // selectedAssetToolStripMenuItem
             // 
