547f68c507e378caab361c3bf9bc7561d516988c
xpouyat <xpouyat@microsoft.com>
2015-03-18 10:25:08 +0100

    Fixed an issue with import credentials

--- AMSExplorer/AMSLogin.cs
+++ AMSExplorer/AMSLogin.cs
@@ -332,10 +332,9 @@ namespace AMSExplorer
                             CredentialsList.Add(att.Attribute("OtherAPIServer").Value.ToString());
                             CredentialsList.Add(att.Attribute("OtherScope").Value.ToString());
                             CredentialsList.Add(att.Attribute("OtherACSBaseAddress").Value.ToString());
-                            if (version >= new Version("1.1"))
+                            if ((version == new Version("1.1")) && (att.Attribute("OtherManagementPortal")) != null)
                             {
-                                CredentialsList.Add(att.Attribute("OtherACSBaseAddress").Value.ToString() + "|" + att.Attribute("OtherManagementPortal").Value.ToString());
-
+                                CredentialsList.Add(att.Attribute("OtherAzureEndpoint").Value.ToString() + "|" + att.Attribute("OtherManagementPortal").Value.ToString());
                             }
                             else if (att.Attribute("OtherAzureEndpoint") != null)
                             {
