138a20f1017bbdd8ac646150b837f2ee03976765
xpouyat <xpouyat@hotmail.com>
2015-05-28 01:10:02 +0200

    Notify icon update

--- AMSExplorer/Mainform.Designer.cs
+++ AMSExplorer/Mainform.Designer.cs
@@ -4212,6 +4212,7 @@
             // 
             // notifyIcon1
             // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
