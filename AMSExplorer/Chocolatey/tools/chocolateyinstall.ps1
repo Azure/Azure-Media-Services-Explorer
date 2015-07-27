82e3740665a6bd4e0ade717d407e0b542ed7a946
xpouyat <xpouyat@hotmail.com>
2015-07-27 15:18:22 +0200

    Update to Chocolatey packages

---
+++ AMSExplorer/Chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'amsexplorer'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://amsexplorer.blob.core.windows.net/release/AMSExplorerSetup_v3.27.0.0.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs    = "/v/qn"
+  registryUninstallerKey = '{4118FAF1-F307-42DF-9DF3-707E94331841}'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ AMSExplorer/Chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'amsexplorer'
+$registryUninstallerKeyName = '{4118FAF1-F307-42DF-9DF3-707E94331841}'
+$msiProductCodeGuid = '{4118FAF1-F307-42DF-9DF3-707E94331841}'
+$shouldUninstall = $true
+
+$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName" 
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'MSI' 
+$silentArgs = "$msiProductCodeGuid /qn /norestart"
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+$file = ''
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
