9937df74c1b7f1ee9cbe0bf0171d300540dccbf0
xpouyat <xpouyat@microsoft.com>
2016-07-12 15:58:28 +0200

    Fixes a typo in a warning message

--- AMSExplorer/EncodingAMEStandard.designer.cs
+++ AMSExplorer/EncodingAMEStandard.designer.cs
@@ -304,8 +304,8 @@
             this.label4KWarning.Name = "label4KWarning";
             this.label4KWarning.Size = new System.Drawing.Size(387, 23);
             this.label4KWarning.TabIndex = 73;
-            this.label4KWarning.Tag = "Warning : you should use a S1 Media RU for 4K";
-            this.label4KWarning.Text = "Warning : you should use a S1 Media RU for 4K";
+            this.label4KWarning.Tag = "Warning : you should use a S3 Media RU for 4K";
+            this.label4KWarning.Text = "Warning : you should use a S3 Media RU for 4K";
             this.label4KWarning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
