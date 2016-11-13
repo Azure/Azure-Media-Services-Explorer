5c9fba7d73d09e01120888ef972ac295d970672a
Shige <shigeyf@microsoft.com>
2016-11-14 01:10:44 +0900

    Added "JaJp".

--- AMSExplorer/IndexerV2.cs
+++ AMSExplorer/IndexerV2.cs
@@ -47,9 +47,10 @@ namespace AMSExplorer
             new Item("German", "DeDe"),
             new Item("Italian", "ItIt"),
             new Item("Portuguese", "PtBr"),
-            new Item("Arabic (Egyptian)", "ArEg") };
+            new Item("Arabic (Egyptian)", "ArEg"),
+            new Item("Japanese", "JaJp") };
 
-        public IndexerOptionsVar IndexerGenerationOptions
+    public IndexerOptionsVar IndexerGenerationOptions
         {
             get
             {
