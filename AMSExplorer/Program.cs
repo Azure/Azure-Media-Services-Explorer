9f1734923a1f70f7786562d2ecad9dac2c1a6962
xpouyat@microsoft.com <xpouyat@microsoft.com>
2021-05-21 19:19:47 +0200

    Add support for Application Insights

--- AMSExplorer/Program.cs
+++ AMSExplorer/Program.cs
@@ -56,7 +56,8 @@ namespace AMSExplorer
         [STAThread]
         private static void Main(string[] args)
         {
-
+            Telemetry.StartTelemetry("4e7e1289-8b4f-4237-af00-c8d2b53ba1b2");
+            Telemetry.TrackEvent("ApplicationStart");
             /*
             
             .net v5 :
---
+++ AMSExplorer/Telemetry.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMSExplorer
+{
+    public static class Telemetry
+    {
+        private static string TelemetryKey = "4e7e1289-8b4f-4237-af00-c8d2b53ba1b2";
+
+        private static TelemetryClient _telemetry;
+
+        public static bool Enabled { get; set; } = true;
+
+        private static TelemetryClient GetAppInsightsClient()
+        {
+            var config = new TelemetryConfiguration();
+            config.InstrumentationKey = TelemetryKey;
+            config.TelemetryChannel = new Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel.ServerTelemetryChannel();
+            //config.TelemetryChannel = new Microsoft.ApplicationInsights.Channel.InMemoryChannel(); // Default channel
+            config.TelemetryChannel.DeveloperMode = Debugger.IsAttached;
+#if DEBUG
+            config.TelemetryChannel.DeveloperMode = true;
+#endif
+            TelemetryClient client = new TelemetryClient(config);
+            client.Context.Component.Version = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            client.Context.Session.Id = Guid.NewGuid().ToString();
+            client.Context.User.Id = (Environment.UserName + Environment.MachineName).GetHashCode().ToString();
+            client.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+            return client;
+        }
+
+        public static void SetUser(string user)
+        {
+            _telemetry.Context.User.AuthenticatedUserId = user;
+        }
+
+        public static void TrackPageView(string pageName)
+        {
+            if (Enabled)
+            {
+                _telemetry.TrackPageView(pageName);
+            }
+        }
+
+        public static void TrackEvent(string key, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null)
+        {
+            if (Enabled)
+            {
+                _telemetry.TrackEvent(key, properties, metrics);
+            }
+        }
+
+        public static void TrackException(Exception ex)
+        {
+            if (ex != null && Enabled)
+            {
+                var telex = new Microsoft.ApplicationInsights.DataContracts.ExceptionTelemetry(ex);
+                _telemetry.TrackException(telex);
+                Flush();
+            }
+        }
+
+        internal static void Flush()
+        {
+            _telemetry.Flush();
+        }
+
+
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            Telemetry.TrackException(ex);
+        }
+
+        private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Telemetry.TrackException(e.Exception);
+        }
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            Telemetry.TrackEvent("Application exited");
+        }
+
+        public static void StartTelemetry(string telemetryKey)
+        {
+            TelemetryKey = telemetryKey;
+            if (_telemetry == null)
+            {
+                _telemetry = GetAppInsightsClient();
+            }
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            Telemetry.TrackEvent("Application started");
+        }
+
+    }
+}
