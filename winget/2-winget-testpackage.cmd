45581a733da19e3e53180a5d6c7f65b3cf9dfdc3
Xavier Pouyat <xpouyat@users.noreply.github.com>
2023-07-26 17:15:46 +0200

    winget update

--- winget/2-winget-testpackage.cmd
+++ winget/2-winget-testpackage.cmd
@@ -1,2 +1,2 @@
-winget validate --manifest C:\Repos\Azure-Media-Services-Explorer\winget\manifests\m\Microsoft\AzureMediaServicesExplorer\5.7.1
-winget install -m C:\Repos\Azure-Media-Services-Explorer\winget\manifests\m\Microsoft\AzureMediaServicesExplorer\5.7.1
+winget validate --manifest C:\Repos\Azure-Media-Services-Explorer\winget\manifests\m\Microsoft\AzureMediaServicesExplorer\5.7.2
+winget install -m C:\Repos\Azure-Media-Services-Explorer\winget\manifests\m\Microsoft\AzureMediaServicesExplorer\5.7.2
